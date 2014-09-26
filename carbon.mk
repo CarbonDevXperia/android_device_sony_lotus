e24bb8cd14b3599efc2a31da3ed6d03d80de5c0c
Info4Geek <admin@info4geek.com>
2014-09-26 20:38:06 +0200

    Carbonize

---
+++ carbon.mk
@@ -0,0 +1,11 @@
+$(call inherit-product, device/sony/kumquat/full_kumquat.mk)
+
+# Inherit Carbon common Phone stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+
+PRODUCT_NAME := carbon_lotus
+PRODUCT_DEVICE := lotus
